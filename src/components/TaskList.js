f3ea386a73618e2649db32c883457078d67ddfaf
Christian James <kwechristian@gmail.com>
2024-08-12 20:05:14 +0300

    fix kiasi

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import NewTaskForm from "./NewTaskForm";
+import Task from "./Task";
 
 function TaskList() {
+  const [tasks, setTasks] = useState([]);
+  const [categories] = useState(["Personal", "Work", "All"]);
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const onDeleteTask = (taskToDelete) => {
+    setTasks(tasks.filter((task) => task.text !== taskToDelete.text));
+  };
+
+  const onTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      <NewTaskForm categories={categories} onTaskFormSubmit={onTaskFormSubmit} />
+      {tasks.map((task) => (
+        <Task
+          key={task.text}
+          text={task.text}
+          category={task.category}
+          onDeleteTask={() => onDeleteTask(task)}
+        />
+      ))}
     </div>
   );
 }
